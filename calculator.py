bec887b194d266decc9d31675054c5768f3a12d0
zakkymustafa <zakiyamustafazm@gmail.com>
2019-04-22 21:29:41 +0300

    python calculator

---
+++ calculator.py
@@ -0,0 +1,35 @@
+def add(a,b):
+	answer= a+b
+	return answer
+
+
+def modulus(k,l):
+	answer=k%l
+	return answer
+
+
+def division(j,g):
+	answer=j/g
+	return answer
+
+
+def wholenumber(h,r):
+	answer=h//r
+	return answer
+
+
+def multiply(p,w):
+	answer= p*w 
+	return answer
+
+def percentage(a,b):
+	answer=(a/b)*100
+	return answer
+
+def subtract (a,b):
+	answer= a-b
+	return answer 
+
+def exponent(a,b):
+	answer= a**b
+	return answer
